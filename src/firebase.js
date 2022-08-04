2ecd204b183227f4fa1eb7469c62fc3efddccf4b
harisilwal <harisilwal.1991@gmail.com>
2022-08-03 19:54:03 -0700

    ff

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,7 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDgRlUFsanyNwXQyEZAee00VHjq9Ic_KMk",
-  authDomain: "coveredcall-4d8ef.firebaseapp.com",
-  projectId: "coveredcall-4d8ef",
-  storageBucket: "coveredcall-4d8ef.appspot.com",
-  messagingSenderId: "559583776109",
-  appId: "1:559583776109:web:fb8d42b736496f02b51e64",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
